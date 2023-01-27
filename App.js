a4c49c6cbd7b25eb08daede4551ba59f65b80521
yu nishisaki <ynishisaki@gmail.com>
2023-01-27 22:26:14 +0900

    feat: 今日の日付表示、薬チェックのボタン追加

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+    const today = new Date();
+
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+    const weekArray = ["日", "月", "火", "水", "木", "金", "土"];
+    const week = weekArray[today.getDay()];
+
+    const [isTookMedicine, setIsTookMedicine] = useState(false);
+
+    function onPressTookMedicine() {
+        setIsTookMedicine(!isTookMedicine);
+    }
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <Text></Text>
+            <Text>today</Text>
+            <Text>
+                {month}月{day}日({week})
+            </Text>
+            <Button
+                onPress={onPressTookMedicine}
+                title={
+                    isTookMedicine
+                        ? "I took my medicine today."
+                        : "take medicine"
+                }
+                color={isTookMedicine ? "gray" : "#841584"}
+                accessibilityLabel='if you took medicine, push this button'
+            />
             <StatusBar style='auto' />
         </View>
     );
