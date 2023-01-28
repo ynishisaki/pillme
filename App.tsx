12c6d4fd4f49293e81888646d21bcb92f025d58c
yu nishisaki <ynishisaki@gmail.com>
2023-01-28 11:52:33 +0900

    feat: AsyncStorageで記録の永続化

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Button, Text, View } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+    // 今日の日付を取得
     const today = new Date();
 
     const month = today.getMonth() + 1;
@@ -10,11 +12,29 @@ export default function App() {
     const weekArray = ["日", "月", "火", "水", "木", "金", "土"];
     const week = weekArray[today.getDay()];
 
+    // 今日薬を飲んだか
     const [isTookMedicine, setIsTookMedicine] = useState(false);
 
     function onPressTookMedicine() {
         setIsTookMedicine(!isTookMedicine);
     }
+
+    // アプリ起動時に今日の薬の記録を取得
+    useEffect(() => {
+        (async () => {
+            const booleanAsString = await AsyncStorage.getItem(
+                "isTookMedicine"
+            );
+            setIsTookMedicine(booleanAsString === "true" ? true : false);
+        })();
+        // 上記の括弧をつけることで即時関数を実行
+    }, []);
+
+    // AsyncStorageに記録を保存
+    useEffect(() => {
+        AsyncStorage.setItem("isTookMedicine", String(isTookMedicine));
+    }, [isTookMedicine]);
+
     return (
         <View style={styles.container}>
             <Text>today</Text>
@@ -25,12 +45,13 @@ export default function App() {
                 onPress={onPressTookMedicine}
                 title={
                     isTookMedicine
-                        ? "I took my medicine today."
+                        ? "I took my medicine today!"
                         : "take medicine"
                 }
                 color={isTookMedicine ? "gray" : "#841584"}
-                accessibilityLabel='if you took medicine, push this button'
+                accessibilityLabel='if you took medicine today, push this button'
             />
+            <Text>{`${isTookMedicine}`}</Text>
             <StatusBar style='auto' />
         </View>
     );
