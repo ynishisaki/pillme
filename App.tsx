d4721fe987911c0b6c98acaf398fc8feaa2db20d
yu nishisaki <ynishisaki@gmail.com>
2023-02-08 22:07:42 +0900

    refactor: DataListから不要な部分削除、デフォルト値を適切なものに指定

--- App.tsx
+++ App.tsx
@@ -5,198 +5,198 @@ import { DateList } from "./components/DateList";
 import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+export interface recordType {
+	[key: string]: {
+		tookMedicine: boolean;
+		haveBleeding: boolean;
+	};
+}
+
+export type datePropertyNameType = (selectedDate: Date) => string;
+
 export default function App() {
-    interface recordType {
-        [key: string]: {
-            tookMedicine: boolean;
-            haveBleeding: boolean;
-        };
-    }
+	const datePropertyName: datePropertyNameType = (selectedDate) => {
+		const offset = selectedDate.getTimezoneOffset();
+		selectedDate = new Date(selectedDate.getTime() - offset * 60 * 1000);
+		return selectedDate.toISOString().split("T")[0];
+	};
 
-    type datePropertyNameType = (selectedDate: Date) => string;
+	const today = datePropertyName(new Date()); // YYYY-DD-MM
 
-    const datePropertyName: datePropertyNameType = (selectedDate) => {
-        const offset = selectedDate.getTimezoneOffset();
-        selectedDate = new Date(selectedDate.getTime() - offset * 60 * 1000);
-        return selectedDate.toISOString().split("T")[0];
-    };
+	const [selectedDatePropertyName, setSelectedDatePropertyName] =
+		useState<string>(today);
 
-    const today = datePropertyName(new Date()); // YYYY-DD-MM
+	// 薬を飲み始めて何日目か
+	const [countDays, setCountDays] = useState<number>(0);
+	const [countBleedingDays, setCountBleedingDays] = useState<number>(0);
 
-    const [selectedDatePropertyName, setSelectedDatePropertyName] =
-        useState<string>(today);
+	// 今日薬を飲んだか
+	const [isTookMedicine, setIsTookMedicine] = useState(false);
+	// 今日出血があったか
+	const [isHaveBleeding, setIsHaveBleeding] = useState(false);
 
-    // 薬を飲み始めて何日目か
-    const [countDays, setCountDays] = useState<number>(0);
-    const [countBleedingDays, setCountBleedingDays] = useState<number>(0);
+	const [dailyRecord, setDailyRecord] = useState<recordType>({
+		[selectedDatePropertyName]: {
+			tookMedicine: false,
+			haveBleeding: false,
+		},
+	});
 
-    // 今日薬を飲んだか
-    const [isTookMedicine, setIsTookMedicine] = useState(false);
-    // 今日出血があったか
-    const [isHaveBleeding, setIsHaveBleeding] = useState(false);
+	function onPressTookMedicine() {
+		setIsTookMedicine(!isTookMedicine);
 
-    const [dailyRecord, setDailyRecord] = useState<recordType>({
-        [selectedDatePropertyName]: {
-            tookMedicine: false,
-            haveBleeding: false,
-        },
-    });
+		setDailyRecord({
+			...dailyRecord,
+			// 今日の記録だけ更新
+			[selectedDatePropertyName]: {
+				tookMedicine: !isTookMedicine, // isTookMedicineは前回の値であることに注意
+				haveBleeding: isHaveBleeding,
+			},
+		});
 
-    function onPressTookMedicine() {
-        setIsTookMedicine(!isTookMedicine);
+		//jsonから全日数分のtrueを数える;
+		const asArray = Object.entries(dailyRecord);
+		const trueDays = asArray.filter(
+			([key, value]) => value.tookMedicine === true
+		).length;
 
-        setDailyRecord({
-            ...dailyRecord,
-            // 今日の記録だけ更新
-            [selectedDatePropertyName]: {
-                tookMedicine: !isTookMedicine, // isTookMedicineは前回の値であることに注意
-                haveBleeding: isHaveBleeding,
-            },
-        });
+		setCountDays(!isTookMedicine ? trueDays + 1 : trueDays); // isTookMedicineは前回の値であることに注意
+	}
 
-        //jsonから全日数分のtrueを数える;
-        const asArray = Object.entries(dailyRecord);
-        const trueDays = asArray.filter(
-            ([key, value]) => value.tookMedicine === true
-        ).length;
+	function onPressHaveBleeding() {
+		setIsHaveBleeding(!isHaveBleeding);
 
-        setCountDays(!isTookMedicine ? trueDays + 1 : trueDays); // isTookMedicineは前回の値であることに注意
-    }
+		setDailyRecord({
+			...dailyRecord,
+			[selectedDatePropertyName]: {
+				tookMedicine: isTookMedicine,
+				haveBleeding: !isHaveBleeding,
+			},
+		});
 
-    function onPressHaveBleeding() {
-        setIsHaveBleeding(!isHaveBleeding);
+		// jsonから今日から直近でtrueで何日連続しているか数える
+		const asArray = Object.entries(dailyRecord);
 
-        setDailyRecord({
-            ...dailyRecord,
-            [selectedDatePropertyName]: {
-                tookMedicine: isTookMedicine,
-                haveBleeding: !isHaveBleeding,
-            },
-        });
+		let i: number = 0;
 
-        // jsonから今日から直近でtrueで何日連続しているか数える
-        const asArray = Object.entries(dailyRecord);
+		for (const [key, value] of asArray.slice(0, -1).reverse()) {
+			if (value.haveBleeding === true) {
+				i++;
+			} else {
+				break;
+			}
+		}
+		setCountBleedingDays(!isHaveBleeding ? i + 1 : 0);
+	}
 
-        let i: number = 0;
+	// AsyncStorageから記録を取得
+	useEffect(() => {
+		(async () => {
+			const recordAsString: string | null = await AsyncStorage.getItem(
+				"record"
+			);
+			// AsyncStorageに記録がないので、デフォルトのdailyRecordを利用する
+			if (recordAsString === null) {
+			}
+			// AsyncStorageから記録取得、stateにsetする
+			else {
+				const record = JSON.parse(recordAsString);
+				const latestProperty = Object.keys(record).at(-1); // 最後のプロパティ名を取得
+				const latestRecord =
+					record[latestProperty as keyof typeof record];
 
-        for (const [key, value] of asArray.slice(0, -1).reverse()) {
-            if (value.haveBleeding === true) {
-                i++;
-            } else {
-                break;
-            }
-        }
-        setCountBleedingDays(!isHaveBleeding ? i + 1 : 0);
-    }
+				// アプリ起動日が、前回起動日と同日だったら、記録を取得
+				if (
+					latestRecord.day === selectedDatePropertyName.slice() // 左辺プロパティ名を取得するもの
+				) {
+					setIsTookMedicine(latestRecord.tookMedicine);
+					setIsHaveBleeding(latestRecord.haveBleeding);
+					setDailyRecord(record);
+				}
+				// アプリ起動日が、前回起動日と異なる日だったら、前回から今日までの記録を追加
+				else {
+					const latestDate = new Date(
+						latestRecord.year,
+						latestRecord.month,
+						latestRecord.day
+					);
+					let lapsedRecord = {};
+					// 時刻まで比較すると、左項は0時0分0秒、右項は現在時刻になることのに注意
+					// while (latestDate.getTime() < today.getTime()) {
+					//     latestDate.setDate(latestDate.getDate() + 1);
+					//     latestRecord.concat({
+					//         month: latestDate.getMonth(),
+					//         day: latestDate.getDate(),
+					//         week: weekArray[latestDate.getDay()],
+					//         tookMedicine: false,
+					//     });
+					// }
 
-    // AsyncStorageから記録を取得
-    useEffect(() => {
-        (async () => {
-            const recordAsString: string | null = await AsyncStorage.getItem(
-                "record"
-            );
-            // AsyncStorageに記録がないので、デフォルトのdailyRecordを利用する
-            if (recordAsString === null) {
-            }
-            // AsyncStorageから記録取得、stateにsetする
-            else {
-                const record = JSON.parse(recordAsString);
-                const latestProperty = Object.keys(record).at(-1); // 最後のプロパティ名を取得
-                const latestRecord =
-                    record[latestProperty as keyof typeof record];
+					setDailyRecord({
+						...record,
+						[selectedDatePropertyName]: {
+							tookMedicine: false,
+							haveBleeding: false,
+						},
+					});
+				}
+			}
+		})();
+		// 上記の括弧をつけることで即時関数を実行
+	}, []);
 
-                // アプリ起動日が、前回起動日と同日だったら、記録を取得
-                if (
-                    latestRecord.day === selectedDatePropertyName.slice() // 左辺プロパティ名を取得するもの
-                ) {
-                    setIsTookMedicine(latestRecord.tookMedicine);
-                    setIsHaveBleeding(latestRecord.haveBleeding);
-                    setDailyRecord(record);
-                }
-                // アプリ起動日が、前回起動日と異なる日だったら、前回から今日までの記録を追加
-                else {
-                    const latestDate = new Date(
-                        latestRecord.year,
-                        latestRecord.month,
-                        latestRecord.day
-                    );
-                    let lapsedRecord = {};
-                    // 時刻まで比較すると、左項は0時0分0秒、右項は現在時刻になることのに注意
-                    // while (latestDate.getTime() < today.getTime()) {
-                    //     latestDate.setDate(latestDate.getDate() + 1);
-                    //     latestRecord.concat({
-                    //         month: latestDate.getMonth(),
-                    //         day: latestDate.getDate(),
-                    //         week: weekArray[latestDate.getDay()],
-                    //         tookMedicine: false,
-                    //     });
-                    // }
+	// AsyncStorageに記録を保存
+	useEffect(() => {
+		AsyncStorage.setItem("record", JSON.stringify(dailyRecord));
+	}, [dailyRecord]);
 
-                    setDailyRecord({
-                        ...record,
-                        [selectedDatePropertyName]: {
-                            tookMedicine: false,
-                            haveBleeding: false,
-                        },
-                    });
-                }
-            }
-        })();
-        // 上記の括弧をつけることで即時関数を実行
-    }, []);
+	// 注意！AsyncStorageを初期化
+	// useEffect(() => {
+	//     (async () => {
+	//         await AsyncStorage.clear();
+	//     })();
+	// }, []);
 
-    // AsyncStorageに記録を保存
-    useEffect(() => {
-        AsyncStorage.setItem("record", JSON.stringify(dailyRecord));
-    }, [dailyRecord]);
+	return (
+		<View style={styles.container}>
+			<DateList dailyRecord={dailyRecord} />
+			<Text style={styles.dateText}>{selectedDatePropertyName}</Text>
 
-    // 注意！AsyncStorageを初期化
-    // useEffect(() => {
-    //     (async () => {
-    //         await AsyncStorage.clear();
-    //     })();
-    // }, []);
+			<Text>{JSON.stringify(dailyRecord)}</Text>
+			<Text>{selectedDatePropertyName}</Text>
 
-    return (
-        <View style={styles.container}>
-            <DateList />
-            <Text style={styles.dateText}>selectedDatePropertyName</Text>
+			{dailyRecord[selectedDatePropertyName]?.tookMedicine ? undefined : (
+				<Text>{`Today is my ${countDays}th medication.`}</Text>
+			)}
 
-            <Text>{JSON.stringify(dailyRecord)}</Text>
-            <Text>{selectedDatePropertyName}</Text>
+			{dailyRecord[selectedDatePropertyName]?.tookMedicine ? (
+				<Text>{`I took ${countDays} times.`}</Text>
+			) : undefined}
 
-            {dailyRecord[selectedDatePropertyName]?.tookMedicine ? undefined : (
-                <Text>{`Today is my ${countDays}th medication.`}</Text>
-            )}
+			<Buttons
+				onPressTookMedicine={onPressTookMedicine}
+				onPressHaveBleeding={onPressHaveBleeding}
+				isTookMedicine={isTookMedicine}
+			/>
 
-            {dailyRecord[selectedDatePropertyName]?.tookMedicine ? (
-                <Text>{`I took ${countDays} times.`}</Text>
-            ) : undefined}
-
-            <Buttons
-                onPressTookMedicine={onPressTookMedicine}
-                onPressHaveBleeding={onPressHaveBleeding}
-                isTookMedicine={isTookMedicine}
-            />
-
-            <Text>{countBleedingDays}</Text>
-            <StatusBar style='auto' />
-        </View>
-    );
+			<Text>{countBleedingDays}</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // alignItems: "center",
-        justifyContent: "center",
-        // marginTop: 50,
-        // marginBottom: 500,
-    },
-    dateText: {
-        fontSize: 40,
-        // lineHeight: 50,
-        borderBottomColor: "black",
-        borderBottomWidth: 1,
-    },
+	container: {
+		flex: 1,
+		// alignItems: "center",
+		justifyContent: "center",
+		// marginTop: 50,
+		// marginBottom: 500,
+	},
+	dateText: {
+		fontSize: 40,
+		// lineHeight: 50,
+		borderBottomColor: "black",
+		borderBottomWidth: 1,
+	},
 });
--- components/DateList.tsx
+++ components/DateList.tsx
@@ -1,309 +1,98 @@
-// import React from "react";
-// import {
-//     SafeAreaView,
-//     View,
-//     VirtualizedList,
-//     StyleSheet,
-//     Text,
-//     StatusBar,
-// } from "react-native";
-
-// type ItemData = {
-//     id: string;
-//     title: string;
-// };
-
-// const getItem = (_data: unknown, index: number): ItemData => ({
-//     id: Math.random().toString(12).substring(0),
-//     title: `12/${index + 1}\nFri`,
-// });
-
-// const getItemCount = (_data: unknown) => 50;
-
-// type ItemProps = {
-//     title: string;
-// };
-
-// const Item = ({ title }: ItemProps) => (
-//     <View style={styles.item}>
-//         <Text style={styles.title}>{title}</Text>
-//     </View>
-// );
-
-// export const DateList = () => {
-//     return (
-//         <View style={styles.container}>
-//             <VirtualizedList
-//                 initialNumToRender={7}
-//                 renderItem={({ item }) => <Item title={item.title} />}
-//                 keyExtractor={(item) => item.id}
-//                 getItemCount={getItemCount}
-//                 getItem={getItem}
-//                 horizontal={true}
-//             />
-//         </View>
-//     );
-// };
-
-// const styles = StyleSheet.create({
-//     container: {
-//         // flex: 0,
-//         marginTop: StatusBar.currentHeight || 0,
-//         backgroundColor: "blue",
-//     },
-//     item: {
-//         backgroundColor: "#e7d2c3",
-//         borderRadius: 100,
-//         height: 50,
-//         width: 50,
-//         alignItems: "center",
-//         justifyContent: "center",
-//         marginVertical: 50,
-//         marginHorizontal: 8,
-//         padding: 0,
-//     },
-//     title: {
-//         fontSize: 9,
-//     },
-// });
-/* eslint-disable react-native/no-inline-styles */
 import * as React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import StepIndicator from "react-native-step-indicator";
-// import Swiper from "react-native-swiper";
-// import { MaterialIcons } from "@expo/vector-icons";
-
-const PAGES = ["Page 1", "Page 2", "Page 3", "Page 4", "Page 5"];
+import { recordType } from "../App";
 
 const firstIndicatorStyles = {
-    stepIndicatorSize: 30,
-    currentStepIndicatorSize: 40,
-    separatorStrokeWidth: 3,
-    currentStepStrokeWidth: 5,
-    separatorFinishedColor: "#4aae4f",
-    separatorUnFinishedColor: "#a4d4a5",
-    stepIndicatorFinishedColor: "#4aae4f",
-    stepIndicatorUnFinishedColor: "#a4d4a5",
-    stepIndicatorCurrentColor: "#ffffff",
-    stepIndicatorLabelFontSize: 15,
-    currentStepIndicatorLabelFontSize: 15,
-    stepIndicatorLabelCurrentColor: "#000000",
-    stepIndicatorLabelFinishedColor: "#ffffff",
-    stepIndicatorLabelUnFinishedColor: "rgba(255,255,255,0.5)",
-    labelColor: "#666666",
-    labelSize: 12,
-    currentStepLabelColor: "#4aae4f",
+	stepIndicatorSize: 30,
+	currentStepIndicatorSize: 40,
+	separatorStrokeWidth: 3,
+	currentStepStrokeWidth: 5,
+	separatorFinishedColor: "#4aae4f",
+	separatorUnFinishedColor: "#a4d4a5",
+	stepIndicatorFinishedColor: "#4aae4f",
+	stepIndicatorUnFinishedColor: "#a4d4a5",
+	stepIndicatorCurrentColor: "#ffffff",
+	stepIndicatorLabelFontSize: 15,
+	currentStepIndicatorLabelFontSize: 15,
+	stepIndicatorLabelCurrentColor: "#000000",
+	stepIndicatorLabelFinishedColor: "#ffffff",
+	stepIndicatorLabelUnFinishedColor: "rgba(255,255,255,0.5)",
+	labelColor: "#666666",
+	labelSize: 12,
+	currentStepLabelColor: "#4aae4f",
 };
 
-const secondIndicatorStyles = {
-    stepIndicatorSize: 30,
-    currentStepIndicatorSize: 40,
-    separatorStrokeWidth: 2,
-    currentStepStrokeWidth: 3,
-    stepStrokeCurrentColor: "#fe7013",
-    stepStrokeWidth: 3,
-    separatorStrokeFinishedWidth: 4,
-    stepStrokeFinishedColor: "#fe7013",
-    stepStrokeUnFinishedColor: "#aaaaaa",
-    separatorFinishedColor: "#fe7013",
-    separatorUnFinishedColor: "#aaaaaa",
-    stepIndicatorFinishedColor: "#fe7013",
-    stepIndicatorUnFinishedColor: "#ffffff",
-    stepIndicatorCurrentColor: "#ffffff",
-    stepIndicatorLabelFontSize: 13,
-    currentStepIndicatorLabelFontSize: 13,
-    stepIndicatorLabelCurrentColor: "#fe7013",
-    stepIndicatorLabelFinishedColor: "#ffffff",
-    stepIndicatorLabelUnFinishedColor: "#aaaaaa",
-    labelColor: "#999999",
-    labelSize: 13,
-    currentStepLabelColor: "#fe7013",
-};
+export const DateList = (props: recordType) => {
+	const arr = Object.entries(props);
+	const arrLength = arr.length;
 
-const thirdIndicatorStyles = {
-    stepIndicatorSize: 25,
-    currentStepIndicatorSize: 30,
-    separatorStrokeWidth: 2,
-    currentStepStrokeWidth: 3,
-    stepStrokeCurrentColor: "#7eaec4",
-    stepStrokeWidth: 3,
-    stepStrokeFinishedColor: "#7eaec4",
-    stepStrokeUnFinishedColor: "#dedede",
-    separatorFinishedColor: "#7eaec4",
-    separatorUnFinishedColor: "#dedede",
-    stepIndicatorFinishedColor: "#7eaec4",
-    stepIndicatorUnFinishedColor: "#ffffff",
-    stepIndicatorCurrentColor: "#ffffff",
-    stepIndicatorLabelFontSize: 0,
-    currentStepIndicatorLabelFontSize: 0,
-    stepIndicatorLabelCurrentColor: "transparent",
-    stepIndicatorLabelFinishedColor: "transparent",
-    stepIndicatorLabelUnFinishedColor: "transparent",
-    labelColor: "#999999",
-    labelSize: 13,
-    currentStepLabelColor: "#7eaec4",
-};
+	const [currentPage, setCurrentPage] = React.useState<number>(4);
 
-const getStepIndicatorIconConfig = ({
-    position,
-    stepStatus,
-}: {
-    position: number;
-    stepStatus: string;
-}) => {
-    const iconConfig = {
-        name: "feed",
-        color: stepStatus === "finished" ? "#ffffff" : "#fe7013",
-        size: 15,
-    };
-    switch (position) {
-        case 0: {
-            iconConfig.name = "shopping-cart";
-            break;
-        }
-        case 1: {
-            iconConfig.name = "location-on";
-            break;
-        }
-        case 2: {
-            iconConfig.name = "assessment";
-            break;
-        }
-        case 3: {
-            iconConfig.name = "payment";
-            break;
-        }
-        case 4: {
-            iconConfig.name = "track-changes";
-            break;
-        }
-        default: {
-            break;
-        }
-    }
-    return iconConfig;
-};
+	const onStepPress = (position: number) => {
+		setCurrentPage(position);
+	};
 
-export const DateList = () => {
-    const [currentPage, setCurrentPage] = React.useState<number>(0);
+	const renderLabel = ({
+		position,
+		label,
+		currentPosition,
+	}: {
+		position: number;
+		stepStatus: string;
+		label: string;
+		currentPosition: number;
+	}) => {
+		return (
+			<Text
+				style={
+					position === currentPosition
+						? styles.stepLabelSelected
+						: styles.stepLabel
+				}>
+				{label}
+			</Text>
+		);
+	};
 
-    const onStepPress = (position: number) => {
-        setCurrentPage(position);
-    };
-
-    const renderViewPagerPage = (data: any) => {
-        return (
-            <View key={data} style={styles.page}>
-                <Text>{data}</Text>
-            </View>
-        );
-    };
-
-    // const renderStepIndicator = (params: any) => (
-    //     <MaterialIcons {...getStepIndicatorIconConfig(params)} />
-    // );
-
-    const renderLabel = ({
-        position,
-        label,
-        currentPosition,
-    }: {
-        position: number;
-        stepStatus: string;
-        label: string;
-        currentPosition: number;
-    }) => {
-        return (
-            <Text
-                style={
-                    position === currentPosition
-                        ? styles.stepLabelSelected
-                        : styles.stepLabel
-                }>
-                {label}
-            </Text>
-        );
-    };
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.stepIndicator}>
-                <StepIndicator
-                    customStyles={firstIndicatorStyles}
-                    currentPosition={currentPage}
-                    labels={[
-                        "Account",
-                        "Profile",
-                        "Band",
-                        "Membership",
-                        "Dashboard",
-                    ]}
-                    renderLabel={renderLabel}
-                    onPress={onStepPress}
-                />
-            </View>
-            {/* <View style={styles.stepIndicator}>
-        <StepIndicator
-          customStyles={secondIndicatorStyles}
-          currentPosition={currentPage}
-          onPress={onStepPress}
-          renderStepIndicator={renderStepIndicator}
-          labels={[
-            'Cart',
-            'Delivery Address',
-            'Order Summary',
-            'Payment Method',
-            'Track',
-          ]}
-        />
-      </View> */}
-            {/* <View style={styles.stepIndicator}>
-        <StepIndicator
-          stepCount={4}
-          customStyles={thirdIndicatorStyles}
-          currentPosition={currentPage}
-          onPress={onStepPress}
-          labels={['Approval', 'Processing', 'Shipping', 'Delivery']}
-        />
-      </View> */}
-            {/* <Swiper
-                style={{ flexGrow: 1 }}
-                loop={false}
-                index={currentPage}
-                autoplay={false}
-                showsButtons
-                onIndexChanged={(page) => {
-                    setCurrentPage(page);
-                }}>
-                {PAGES.map((page) => renderViewPagerPage(page))}
-            </Swiper> */}
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<View style={styles.stepIndicator}>
+				<StepIndicator
+					customStyles={firstIndicatorStyles}
+					currentPosition={currentPage}
+					labels={["1(月)", "2(火)", "3(水)", "4(木)", "today"]}
+					renderLabel={renderLabel}
+					onPress={onStepPress}
+				/>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#ffffff",
-    },
-    stepIndicator: {
-        marginVertical: 50,
-    },
-    page: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    stepLabel: {
-        fontSize: 12,
-        textAlign: "center",
-        fontWeight: "500",
-        color: "#999999",
-    },
-    stepLabelSelected: {
-        fontSize: 12,
-        textAlign: "center",
-        fontWeight: "500",
-        color: "#4aae4f",
-    },
+	container: {
+		flex: 1,
+		backgroundColor: "#ffffff",
+	},
+	stepIndicator: {
+		marginVertical: 50,
+	},
+	page: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	stepLabel: {
+		fontSize: 12,
+		textAlign: "center",
+		fontWeight: "500",
+		color: "#999999",
+	},
+	stepLabelSelected: {
+		fontSize: 12,
+		textAlign: "center",
+		fontWeight: "500",
+		color: "#4aae4f",
+	},
 });
