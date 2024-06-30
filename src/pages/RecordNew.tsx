ed563846bdc3dbee3d463ad7cbce44a74610e021
monyo <96361228+ynishisaki@users.noreply.github.com>
2024-06-30 22:03:29 +0900

    feat: RecordNew

--- src/pages/RecordNew.tsx
+++ src/pages/RecordNew.tsx
@@ -1,17 +1,18 @@
 import { useIsFocused } from "@react-navigation/native";
-import dayjs from "dayjs";
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import DateTimePicker, { DateType } from "react-native-ui-datepicker";
+import { useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import { Calendar, DateData, LocaleConfig } from "react-native-calendars";
+import { MarkedDates } from "react-native-calendars/src/types";
+import { useRecoilState } from "recoil";
 import ContentLayout from "~/components/ContentLayout";
-import { CurrentSheet } from "~/components/record/CurrentSheet";
-import { WeeklyRecord } from "~/components/record/WeeklyRecord";
-import ScreenLayout from "~/template/ScreenLayout";
-// import {`[Calendar](#calendar), [CalendarList](#calendarlist), [Agenda](#agenda)`} from 'react-native-calendars';
-import { Calendar, LocaleConfig } from "react-native-calendars";
-import { useRecoilValue } from "recoil";
-import { HomeTodaysRecord } from "~/components/home/HomeTodaysRecord";
+import CheckBox from "~/components/common/CheckBox";
+import CheckboxTitleText from "~/components/common/CheckboxTitleText";
+import OverviewText from "~/components/common/OverviewText";
+import { getDateWeekStringsForDisplay } from "~/functions/getDateStrings";
+import { judgeIsTomorrowStartsRestPeriod } from "~/functions/judgeIsRestPeriod";
 import { recordState } from "~/states/recordState";
+import { pillColor } from "~/styles/color";
+import ScreenLayout from "~/template/ScreenLayout";
 
 LocaleConfig.locales.jp = {
 	monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
@@ -21,49 +22,120 @@ LocaleConfig.locales.jp = {
 };
 LocaleConfig.defaultLocale = "jp";
 
-// const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
-// const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
-// const workout = {key: 'workout', color: 'green'};
-
-const tookMedicineDot = { key: "tookMedicine", color: "blue", selectedDotColor: "blue" };
-const haveBleedingDot = { key: "haveBleeding", color: "red", selectedDotColor: "red" };
-const isRestPeriodDot = { key: "restPeriod", color: "gray", selectedDotColor: "gray" };
-
 export const Record = () => {
 	const isFocused = useIsFocused();
 
-	const record = useRecoilValue(recordState);
+	const [record, setRecord] = useRecoilState(recordState);
 
-	const selectedDateDots = { "2024-06-01": { selected: true, marked: true, selectedColor: "blue" } };
-	const markedDates = record.dailyRecord.reduce((acc, record, index) => {
-		const { tookMedicine, haveBleeding, isRestPeriod } = record;
+	const [selectedDailyRecordIndex, setSelectedDailyRecordIndex] = useState(0);
+	const [isRecordNone, setIsRecordNone] = useState(false);
 
-		const dots = [];
-		if (tookMedicine) dots.push(tookMedicineDot);
-		if (haveBleeding) dots.push(haveBleedingDot);
-		if (isRestPeriod) dots.push(isRestPeriodDot);
+	// today record
+	const selectedRecord = record.dailyRecord[selectedDailyRecordIndex];
 
-		acc = {
-			...acc,
-			[record.date]: { dots: dots },
+	const selectedDate = record.dailyRecord[selectedDailyRecordIndex].date;
+	const dateWeekStringsForDisplay = getDateWeekStringsForDisplay(selectedDate);
+
+	function handleDayPress(date: DateData) {
+		const pressedDate = date.dateString;
+
+		const index = record.dailyRecord.findIndex((record) => record.date === pressedDate);
+
+		if (index > -1) {
+			setSelectedDailyRecordIndex(index);
+			setIsRecordNone(false);
+			return;
+		}
+		// 記録範囲外の日付を選択した場合の処理
+		setIsRecordNone(true);
+	}
+
+	const markedDates: MarkedDates = record.dailyRecord.reduce((acc, record, index) => {
+		const selectedRecord = {
+			[record.date]: {
+				selected: selectedDate === record.date ? true : false,
+				marked: record.tookMedicine || record.isRestPeriod,
+				selectedColor: "blue",
+				dotColor: record.tookMedicine ? pillColor : "gray",
+			},
 		};
-		return acc;
-	}, selectedDateDots);
+		return {
+			...acc,
+			...selectedRecord,
+		};
+	}, {});
+
+	function handleUpdateRecord(key: string, nextBoolean: boolean, index: number) {
+		let updatedRecord = {
+			...record,
+			dailyRecord: [
+				// 記録更新対象の日以降の記録は削除
+				...record.dailyRecord.slice(0, index).map((dailyRecord) => {
+					return {
+						...dailyRecord,
+						tookMedicine: false,
+						haveBleeding: false,
+						isRestPeriod: false,
+					};
+				}),
+				// 記録更新対象
+				{
+					...record.dailyRecord[index],
+					[key]: nextBoolean,
+				},
+				...record.dailyRecord.slice(index + 1),
+			],
+		};
+
+		const isTomorrowStartsRestPeriod = judgeIsTomorrowStartsRestPeriod(updatedRecord, index);
+
+		if (isTomorrowStartsRestPeriod) {
+			const { stopTakingDays } = record.initialSheetSettings;
+			const updateRecordToIndex = index > stopTakingDays ? index - stopTakingDays : 0;
+
+			updatedRecord = {
+				...updatedRecord,
+				dailyRecord: [
+					...updatedRecord.dailyRecord.slice(0, updateRecordToIndex),
+					// 休薬期間
+					...Array.from({ length: index - updateRecordToIndex }, (_, i) => {
+						return {
+							...updatedRecord.dailyRecord[i],
+							isRestPeriod: isTomorrowStartsRestPeriod,
+						};
+					}),
+					// 記録した日はすでにupdatedRecordに含まれている
+					...updatedRecord.dailyRecord.slice(index),
+				],
+			};
+
+			alertTomorrowRestPeriod();
+		}
+		setRecord(updatedRecord);
+	}
+
+	const alertTomorrowRestPeriod = () =>
+		Alert.alert(
+			"休薬日となりました",
+			`出血の有無に関わらず${record.initialSheetSettings.stopTakingDays}日間休薬します。`,
+			[
+				{
+					text: "OK",
+					style: "default",
+				},
+			]
+		);
 
 	return (
 		<ScreenLayout>
 			{isFocused && (
 				<View style={styles.viewLayout}>
-					<ContentLayout title='現在のシート'>
+					<ContentLayout title='カレンダー'>
 						<Calendar
-							onDayPress={(day) => {
-								console.log("selected day", day);
-							}}
-							markingType={"multi-dot"}
+							// TODO: Pressできる日付を限定する
+							onDayPress={handleDayPress}
 							markedDates={markedDates}
-							style={{
-								height: "100%",
-							}}
+							showSixWeeks
 							theme={{
 								"stylesheet.calendar.header": {
 									dayTextAtIndex0: {
@@ -73,38 +145,39 @@ export const Record = () => {
 										color: "blue",
 									},
 								},
-								"stylesheet.calendar.main": {
-									monthView: {
-										flex: 1,
-										height: "100%",
-										justifyContent: "space-around",
-									},
-									week: {
-										flex: 1,
-										marginVertical: 0,
-										flexDirection: "row",
-										justifyContent: "space-around",
-									},
-									dayContainer: {
-										borderColor: "#f5f5f5",
-										borderWidth: 1,
-										flex: 1,
-									},
-								},
 							}}
 						/>
-						{/* <DateTimePicker
-							mode='multiple'
-							locale='ja'
-							// 120日前〜今日まで選択可能
-							minDate={dayjs().subtract(120, "day")}
-							maxDate={dayjs()}
-							date={selectedDate}
-							// onChange={(params) => setSelectedDate(params.date)}
-						/> */}
 					</ContentLayout>
-					{/* <CurrentSheet />
-					<WeeklyRecord /> */}
+					<ContentLayout title={`${dateWeekStringsForDisplay} の記録`}>
+						<View style={styles.contentLayout}>
+							{isRecordNone ? (
+								<OverviewText type='warn'>服薬記録がありません</OverviewText>
+							) : (
+								<View style={styles.checkBoxLayout}>
+									<CheckBox
+										textComponent={<CheckboxTitleText>服薬</CheckboxTitleText>}
+										type='medicine'
+										size={"md"}
+										isChecked={selectedRecord.tookMedicine}
+										isRestPeriod={selectedRecord.isRestPeriod}
+										onPress={(nextBoolean) =>
+											handleUpdateRecord("tookMedicine", nextBoolean, selectedDailyRecordIndex)
+										}
+									/>
+									<CheckBox
+										textComponent={<CheckboxTitleText>出血</CheckboxTitleText>}
+										type='bleeding'
+										size={"md"}
+										isChecked={selectedRecord.haveBleeding}
+										isRestPeriod={selectedRecord.isRestPeriod}
+										onPress={(nextBoolean) =>
+											handleUpdateRecord("haveBleeding", nextBoolean, selectedDailyRecordIndex)
+										}
+									/>
+								</View>
+							)}
+						</View>
+					</ContentLayout>
 				</View>
 			)}
 		</ScreenLayout>
@@ -114,9 +187,20 @@ export const Record = () => {
 const styles = StyleSheet.create({
 	viewLayout: {
 		flex: 1,
-		justifyContent: "center",
-		// justifyContent: "flex-end",
+		// justifyContent: "center",
+		justifyContent: "flex-end",
 		// alignItems: "center",
-		// rowGap: 50,
+		rowGap: 10,
+	},
+	contentLayout: {
+		// paddingHorizontal: 10,
+		paddingVertical: 10,
+		gap: 10,
+	},
+	checkBoxLayout: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		width: "100%",
+		// height: 100,
 	},
 });
