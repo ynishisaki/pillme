501ceed30a93867e0e4d2a86e84ea83872702344
monyo <96361228+ynishisaki@users.noreply.github.com>
2024-11-24 15:17:53 +0900

    style: add SafeAreaView

--- components/common/screen/ScreenContainer.tsx
+++ components/common/screen/ScreenContainer.tsx
@@ -1,22 +1,27 @@
 import { ReactNode } from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ScreenContainer({ children }: { children: ReactNode }) {
-	return (
-		<ImageBackground source={require("@/assets/bgimage.png")} resizeMode='cover' style={styles.backgroundImage}>
-			<View style={[styles.container]}>{children}</View>
-		</ImageBackground>
-	);
+  return (
+    <ImageBackground
+      source={require("@/assets/bgimage.png")}
+      resizeMode="cover"
+      style={styles.backgroundImage}
+    >
+      <SafeAreaView style={[styles.container]}>{children}</SafeAreaView>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-	backgroundImage: {
-		flex: 1,
-		justifyContent: "center",
-		width: "100%",
-		height: "100%",
-	},
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
 });
